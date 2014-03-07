4b9bd9380f44200345ac052caa8c6a2866dba5b4
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2014-03-07 15:09:02 +0100

    I'm hungry

--- tests/python/dummy_test.py
+++ tests/python/dummy_test.py
@@ -9,7 +9,7 @@ import nose
 
 def test_that_index_page_is_working():
     """ Test to see if True is equal True """
-    assert True == True
+    assert True == False
 
 if __name__ == '__main__':
     nose.main()
