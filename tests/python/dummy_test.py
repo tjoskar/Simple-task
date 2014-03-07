9dd95a0f53ba2d9199b0f73f41f44239bbe4dd9c
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2014-03-07 13:26:36 +0100

    I expected something different.

---
+++ tests/python/dummy_test.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""
+Testfall för att se om true är true
+"""
+
+import nose
+
+def test_that_index_page_is_working():
+    """ Test to see if True is equal True """
+    assert True == True
+
+if __name__ == '__main__':
+    nose.main()
