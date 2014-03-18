844585d0ac467e23ad578b52d9ef4b6c9f4cd3e8
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2014-03-18 09:23:42 +0100

    Test that true is true

--- tests/python/dummy_test.py
+++ tests/python/dummy_test.py
@@ -11,5 +11,9 @@ def test_that_index_page_is_working():
     """ Test to see if True is equal True """
     assert True == True
 
+def test_that_true_is_true():
+    """ Test to see if True is equal True """
+    assert True == True
+
 if __name__ == '__main__':
     nose.main()
